ca820e3e6646a72bd2f1add10e40cf96d1065d02
Paula Krupa <paula.krupa@gmail.com>
2021-02-09 23:54:16 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from "react";
 import './App.css';
 
-let myDate = new Date()
-let myHour = myDate.getHours().toString();
-let myMinute = myDate.getMinutes().toString();
-let mySecond = myDate.getSeconds().toString();
+// let myDate = new Date()
+// let myHour = myDate.getHours().toString();
+// let myMinute = myDate.getMinutes().toString();
+// let mySecond = myDate.getSeconds().toString();
 
-let myTime = (mySecond.length === 1) ? (myHour + ":" + myMinute + ":0" + mySecond) : (myHour + ":" + myMinute + ":" + mySecond);
+// let myTime = (mySecond.length === 1) ? (myHour + ":" + myMinute + ":0" + mySecond) : (myHour + ":" + myMinute + ":" + mySecond);
 
-let handleClick = () => {
-    myDate = new Date()
-}
+// let handleClick = () => {
+//     myDate = new Date()
+// }
 
-function App() {
-    return (
-        <div className="App">
-          {myTime}
-          <div className="RefreshButton" onClick={handleClick}>Refresh</div>
-        </div>
-    );
+class App extends React.Component {
+
+    state = {
+        myDate: new Date(),
+        myHour: this.myDate.getHours().toString(),
+        myMinute: this.myDate.getMinutes().toString(),
+        mySecond: this.myDate.getSeconds().toString()
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.myDate}
+                <div className="RefreshButton">Refresh</div>
+            </div>
+        );
+    }
 }
 
 export default App;
